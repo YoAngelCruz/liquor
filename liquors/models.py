c95b3854891424404db65cc2746db75f0d6404bc
angel <angelcruz.feb@gmail.com>
2023-05-29 17:08:11 -0600

    create votes

--- liquors/models.py
+++ liquors/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from dateutil.relativedelta import relativedelta
+from django.conf import settings
 
 class Liquor(models.Model):
     destilado = models.CharField(max_length=50,default='alcohol',blank=True) 
@@ -13,3 +14,8 @@ class Liquor(models.Model):
     caducidad = models.DateField (default= timezone.datetime.today() + relativedelta(months=+3))
     edicion = models.CharField (max_length=50,default='Standar')
     precio = models.FloatField (default=1000)
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    liquor = models.ForeignKey('liquors.Liquor', related_name='votes', on_delete=models.CASCADE)
--- liquors/schema.py
+++ liquors/schema.py
@@ -1,19 +1,28 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Liquor
+from .models import Liquor, Vote
+from users.schema import UserType
+
 
 
 class LiquorType(DjangoObjectType):
     class Meta:
         model = Liquor
 
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
+
 
 class Query(graphene.ObjectType):
     liquors = graphene.List(LiquorType)
+    votes = graphene.List(VoteType)
 
     def resolve_liquors(self, info, **kwargs):
         return Liquor.objects.all()
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 class CreateLiquor(graphene.Mutation):
     id = graphene.Int()
@@ -25,6 +34,7 @@ class CreateLiquor(graphene.Mutation):
     tipoEnvase = graphene.String()
     edicion = graphene.String()
     precio = graphene.Float()
+    posted_by = graphene.Field(UserType)
 
     #2
     class Arguments:
@@ -38,6 +48,9 @@ class CreateLiquor(graphene.Mutation):
         precio = graphene.Float()
     #3
     def mutate(self, info, destilado, nombre, description, paisOrigen, size, tipoEnvase, edicion, precio):
+        
+        user = info.context.user or None
+
         liquor = Liquor(destilado=destilado, 
                         nombre=nombre, 
                         description=description, 
@@ -45,7 +58,9 @@ class CreateLiquor(graphene.Mutation):
                         size=size, 
                         tipoEnvase=tipoEnvase, 
                         edicion=edicion, 
-                        precio=precio)
+                        precio=precio,
+                        posted_by=user,
+                        )
         liquor.save() #Insert into liquor (...) Values (...)
 
         return CreateLiquor(
@@ -57,12 +72,38 @@ class CreateLiquor(graphene.Mutation):
             size=liquor.size,
             tipoEnvase=liquor.tipoEnvase,
             edicion=liquor.edicion,
-            precio=liquor.precio
+            precio=liquor.precio,
+            posted_by=liquor.posted_by,
         )
+    
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    liquor = graphene.Field(LiquorType)
+
+    class Arguments:
+        liquor_id = graphene.Int()
+    
+    def mutate(self, info, liquor_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('You must be logged to vote!')
+
+        liquor = Liquor.objects.filter(id=liquor_id).first()
+        if not liquor:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user=user,
+            liquor=liquor,
+        )
+
+        return CreateVote(user=user, liquor=liquor)
 
 
 #4
 class Mutation(graphene.ObjectType):
     create_liquor = CreateLiquor.Field()
+    create_vote = CreateVote.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
